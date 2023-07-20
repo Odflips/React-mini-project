59e5d6f3eaccd4f4cf1940c6e542889bbd6198ff
oliver leal <oliver.lealch@gmail.com>
2023-07-19 21:22:58 -0300

    React mini protecto

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Created by <span className="font-bold">Oliver leal</span> -
+        devChallenges.io
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
